95a2295156f4a35e702f9415ee9ac3993f5210de
oomusou <oomusou@gmail.com>
2016-05-08 14:59:32 +0800

    重構 LogisticsFactory

--- app/Services/LogisticsFactory.php
+++ app/Services/LogisticsFactory.php
@@ -2,15 +2,16 @@
 
 namespace App\Services;
 
+use App;
 use Illuminate\Support\Collection;
 
 class LogisticsFactory
 {
-    public static function create(int $companyNo = 0) : LogisticsInterface
+    public static function bind(int $companyNo = 0)
     {
         $lut = config('app.logistics');
         $className = Collection::make($lut)->get($companyNo, BlackCat::class);
 
-        return new $className;
+        App::bind(LogisticsInterface::class, $className);
     }
 }
